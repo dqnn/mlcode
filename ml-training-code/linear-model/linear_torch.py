f3904dea763226d2b649414c9dfe69f4cca9ecd5
dqdx <dqdxby@gmail.com>
2025-07-08 17:10:50 -0700

    add more

---
+++ ml-training-code/linear-model/linear_torch.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+from d2l import torch as d2l
+d2l.use_svg_display()
