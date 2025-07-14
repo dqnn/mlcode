75e35c79fe24fc3bff55d865e8634f07de4540eb
dqdx <dqdxby@gmail.com>
2025-07-14 00:09:53 -0700

    add more

--- ml-training-code/linear-model/utils.py
+++ ml-training-code/linear-model/utils.py
@@ -5,7 +5,8 @@ from d2l import torch as d2l
 from torch.utils import data
 from torchvision import transforms
 import matplotlib.pyplot as plt
-
+import Animator
+    
 #  train code
 def train_epoch_ch3(net, train_iter, loss, updater):
     if isinstance(net, torch.nn.Module):
@@ -27,47 +28,6 @@ def train_epoch_ch3(net, train_iter, loss, updater):
     #  [train loss, train accuracy]
     return metric[0]/metric[2], metric[1]/metric[2]
 
-#draw the graph
-
-class Animator:
-    def __init__(self, xlabel=None, ylabel=None, legend =None, xlim=None, 
-                 ylim = None, xscale='linear', yscale='linear', fmts=('-', 'm--', 'g-', 'r:'), 
-                 nrows =1, ncols=1, figsize=(3.5, 2.5)):
-        from d2l import torch as d2l
-        d2l.use_svg_display()
-        # 增量地绘制多条线
-        if legend is None:
-            legend = []
-        
-        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
-        if nrows * ncols == 1:
-            self.axes = [self.axes, ]
-        # 使用lambda函数捕获参数
-        self.config_axes = lambda: d2l.set_axes(
-            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
-        self.X, self.Y, self.fmts = None, None, fmts
-    def add(self, x, y):
-        # 向图表中添加多个数据点
-        if not hasattr(y, "__len__"):
-            y = [y]
-        n = len(y)
-        if not hasattr(x, "__len__"):
-            x = [x] * n
-        if not self.X:
-            self.X = [[] for _ in range(n)]
-        if not self.Y:
-            self.Y = [[] for _ in range(n)]
-        for i, (a, b) in enumerate(zip(x, y)):
-            if a is not None and b is not None:
-                self.X[i].append(a)
-                self.Y[i].append(b)
-        self.axes[0].cla()
-        for x, y, fmt in zip(self.X, self.Y, self.fmts):
-            self.axes[0].plot(x, y, fmt)
-        self.config_axes()
-        display.display(self.fig)
-        display.clear_output(wait=True)
-    
 #  loss is cross_entropy
 def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater): #@save
     """训练模型（定义见第3章）"""
